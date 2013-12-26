d277dd3ff0b73fca2bdcea052497b569b0c63920
DaveGordon_cp <SND\DaveGordon_cp>
2013-12-26 00:46:45 +0000

    Initial Upload

---
+++ MeteorTools/MeteorTools/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MeteorTools.MeteorTools
+{
+    static class GuidList
+    {
+        public const string guidMeteorToolsPkgString = "e38885f5-7924-450c-bc93-945cc407115a";
+        public const string guidMeteorToolsCmdSetString = "f40f4259-ec15-4bf8-bedf-7b86eaadf1f3";
+
+        public static readonly Guid guidMeteorToolsCmdSet = new Guid(guidMeteorToolsCmdSetString);
+    };
+}
---
+++ MeteorTools/MeteorTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MeteorTools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MeteorTools")]
+[assembly: AssemblyProduct("MeteorTools")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("MeteorTools_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b8d06b42353aaa3aa9f5ad6b7b3d7f23c939f61f485d42d046839b6818eba38f365dfafe5e764a303ca37d86ac4da904aaeccb21b338748dbd93208cfa2c28130ce4ca3da3eca5ae4536778ffcf554f141f053988a123960cba70f039359a0408a540d68683d63c28614b889a6cedf679cd9d9eb2851da059e6785dca3162c6")]
+[assembly: InternalsVisibleTo("MeteorTools_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b8d06b42353aaa3aa9f5ad6b7b3d7f23c939f61f485d42d046839b6818eba38f365dfafe5e764a303ca37d86ac4da904aaeccb21b338748dbd93208cfa2c28130ce4ca3da3eca5ae4536778ffcf554f141f053988a123960cba70f039359a0408a540d68683d63c28614b889a6cedf679cd9d9eb2851da059e6785dca3162c6")]
